65897798d3694ed6d66cc42a696351c3b28e6032
fncreative <dann.frey@me.com>
2018-03-13 19:48:20 -0700

    Update registration.sql

--- registration.sql
+++ registration.sql
@@ -1,50 +1,56 @@
---create stored procedure for adding new people
-Create proc [dbo].[usp_Register]
-@lastname nvarchar(255),
-@firstname nvarchar(255)=null,
-@email nvarchar(255),
-@Phone nvarchar(13) = null
-As
---check to make sure person doesn't exit
-If not exists
-   (Select PersonKey
-      From Person
-	  Where PersonLastName=@LastName
-	  And PersonEmail = @Email)
-Begin--begin the block of if not exists
---begin the transaction
-Begin tran
---begin the try catch
-Begin Try
---insert into tables
-Insert into Person
-(
-   PersonLastName, 
-   PersonFirstName, 
-   PersonEmail, 
-   PersonDateAdded, 
-   PersonPrimaryPhone
-)
-Values
-(
-   @LastName,
-   @FirstName,
-   @Email,
-   getDate(),
-   @Phone
-)
---get the current person key
-Declare @PersonKey int
-Set @PersonKey=IDENT_CURRENT('Person')
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Bakery.Models;
 
-  Commit tran
-End try
-Begin Catch
-Rollback tran
-Return -1
-End catch
-End
-Else
-Begin
-Return -1
-End
+namespace Bakery.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        BakeryEntities db = new BakeryEntities();
+
+        // GET: Registration
+        public ActionResult Index()
+        {
+            return View(db.People.ToList());
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        
+        public ActionResult Register([Bind(Include ="LastName, FirstName, Email, " +
+                                                    "Phone")] NewPerson p)
+        {
+            int result = db.uRegister(p.LastName, p.FirstName, p.Email,
+                                         p.Phone);
+
+            if(result != -1)
+            {
+                return RedirectToAction("Success");
+            }
+
+            return RedirectToAction("Failure");
+        }
+
+        public ActionResult Success()
+        {
+            Message successMsg =  new Message();
+            successMsg.MessageText = "Thanks for registering.";
+            return View("Result", successMsg);
+        }
+
+        public ActionResult Failure()
+        {
+            Message failureMsg = new Message();
+            failureMsg.MessageText = "Sorry, but something seems to have gone wrong with the registration.";
+            return View("Result", failureMsg);
+        }
+    }
+}
